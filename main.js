c167b13b877aaf916c39895ae80e92dbe5be5cae
JPantana <joshpantana@me.com>
2019-03-18 13:49:39 -0500

    ok its working but I'm probably gonna break my code here

--- main.js
+++ main.js
@@ -4,11 +4,13 @@ const printToDom = (divId, textToPrint) => {
 };
 
 
-const domStringBuilder = (determineConverter, celOrFah) => {
+const domStringBuilder = (determineConverter, celOrFah, toCelsius, toFahrenheit) => {
     let domString = '';
         domString += `<div class="printedDomCard">`;
         domString += `<h2>${determineConverter}</h2>`;
         domString += `<h2>${celOrFah}</h2>`;
+        // could do an if statement here to say converted to then the opp. of what's right above this line
+        domString += `<h2>Converted to</h2>`;
         domString += `</div>`;
     // call printToDom function
     printToDom('tempOutput', domString);
@@ -30,21 +32,36 @@ const determineConverter = (e) => {
         const celOrFah = () => {
             if (document.getElementById('c').checked === true) {
                 console.log('Celcius');
+                toCelsius();
                 return 'Celcius';
             } else if (document.getElementById('f').checked === true) {
                 console.log('Fahrenheit');
+                toFahrenheit();
                 return 'Fahrenheit';
             }
         };
         celOrFah();
         domStringBuilder(inputValue, celOrFah());
+
     };  
+
+
+
+    const toCelsius =  () => {
+        const fTemp = document.getElementById('tempInput').value;
+        console.log(Math.round((fTemp - 32) * 5/9));
+        return (Math.round((fTemp - 32) * 5/9));
+    };
+    
+    const toFahrenheit =  () => {
+        const cTemp = document.getElementById('tempInput').value;
+        console.log(Math.round(((cTemp * 9) / 5) + 32));
+        return (Math.round(((cTemp * 9) / 5) + 32));
+    };
 
 // Assign a function to be executed when the button is clicked
 const buttonEvents = () => {
     document.getElementById('convertBtn', 'c' || 'f').addEventListener('click', determineConverter);
-    // document.getElementById('c').addEventListener('click', determineConverter);
-    // document.getElementById('f').addEventListener('click', determineConverter);
 };
 
 const init = () => {
@@ -54,14 +71,3 @@ init();
 
 
 
-const toCelsius =  () => {
-    const fahrenheit = 20;
-    const tempC = (fahrenheit - 32) * 5/9;
-    console.log(tempC);
-};
-
-const toFahrenheit =  () => {
-    const celcius = 20;
-    const tempF = (celcius * 9) / 5 + 32;
-    console.log(tempF);
-};
