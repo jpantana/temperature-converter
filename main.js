07d3d2eeb8ed050825164839cc634e8ab4e2a33f
JPantana <joshpantana@me.com>
2019-03-18 08:16:27 -0500

    console now prints temperature input and radio input selected

--- main.js
+++ main.js
@@ -6,11 +6,11 @@ const printToDom = (divId, textToPrint) => {
 
 const domStringBuilder = () => {
     let domString = '';
-    divId.forEach(() => {
-        domString += `<div class="">`;
-        domString += `<h2></h2>`;
-        domString += `<h2></h2>`;
-    });
+    // divId.forEach(() => {
+    //     domString += `<div class="">`;
+    //     domString += `<h2></h2>`;
+    //     domString += `<h2></h2>`;
+    // });
     printToDom('tempOutput', domString);
 };
 
@@ -33,33 +33,29 @@ const button = document.getElementById("converter");
 // This function should determine which conversion should
 // happen based on which radio button is selected.
 const determineConverter = (e) => {
-    // const buttonId = e.target.id;
-    // const selectedTemps = [];
     const inputValue = document.getElementById('tempInput').value;
     console.log(inputValue);
-    // console.log("event", e);
 };
 
 const clickCatcher = () => {
     if (document.getElementById('c').checked === true) {
        console.log('Celcius');
-       selectedTemps.push(inputValue.value);
-       return 'Celcius'
     } else if (document.getElementById('f').checked === true) {
         console.log('Fahrenheit');
-        selectedTemps.push(clickCatcher);
-        return 'Fahrenheit'
     }
 };
+
 
 
 
 // Assign a function to be executed when the button is clicked
 const buttonEvents = () => {
     document.getElementById('tempOutput').addEventListener("click", determineConverter);
+    document.getElementById('c').addEventListener("click", clickCatcher);
+    document.getElementById('f').addEventListener("click", clickCatcher);
 };
 
 const init = () => {
-    // domStringBuilder();
+    buttonEvents();
 };
 init();
